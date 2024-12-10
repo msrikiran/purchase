2ecfb2afde8b656063de322b83eab3ba2c3313b6
msrikiran <msrikiran@gmail.com>
2024-12-10 12:10:57 +0530

    purchase app header footer added

--- apps/purchase/src/app/app.component.ts
+++ apps/purchase/src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HomePageComponent, ProductListComponent],
+  imports: [
+    RouterModule,
+    HomePageComponent,
+    ProductListComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
