8cad4dae096e74fc3462de0642c25ce6d74ea16a
msrikiran <msrikiran@gmail.com>
2024-12-10 11:59:04 +0530

    home-page and product list components are created

--- apps/purchase/src/app/app.component.ts
+++ apps/purchase/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HomePageComponent, ProductListComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
