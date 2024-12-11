491e077ece3c614836e3d48e834a0c3e25bdc0f7
msrikiran <msrikiran@gmail.com>
2024-12-11 23:00:33 +0530

    instead of browsermodule added provideanimations in config file

--- apps/purchase/src/app/app.component.ts
+++ apps/purchase/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
-import { UiComponent } from '@purchase/ui';
 import { BannerComponent } from '../../../../libs/ui/src/lib/banner/banner.component';
+import { CommonModule } from '@angular/common';
 @Component({
   standalone: true,
   imports: [
+    CommonModule,
     RouterModule,
     HomePageComponent,
     ProductListComponent,
--- apps/purchase/src/app/pages/home-page/home-page.component.ts
+++ apps/purchase/src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { AccordionModule } from 'primeng/accordion';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, AccordionModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
